ddda78b78a9a2ba38bf7a36740e72a525aedc90d
zeenayouhan <zinayouhan33@gmail.com>
2020-04-13 19:51:31 +0530

    Registration-user-3

--- lib/Register-User.dart
+++ lib/Register-User.dart
@@ -42,4 +42,63 @@ class _RegisterUserState extends State<RegisterUser> {
           color: Colors.white,
           
           
-          padding: const EdgeInsets.only(left:20,top: 80,right:20),}}
+          padding: const EdgeInsets.only(left:20,top: 80,right:20),
+          child:SingleChildScrollView(
+
+            child: Form(       
+               
+            
+            child: Column(
+              
+              
+                children: <Widget>[
+                  Text("Step 2/2", style: TextStyle(fontSize: 30,color: Colors.lightBlue),textAlign: TextAlign.center, ),
+                  
+                  
+                  TextFormField(
+                    
+                    
+                    decoration: InputDecoration(
+                      border: const OutlineInputBorder(borderSide: const BorderSide(color: Colors.grey,width: 1.0)),
+                      
+                        labelText: 'Phone Number', hintText: "0XXXXXXX"),
+                        
+                  ),
+                   RaisedButton(
+                    child: Text("Verify",style:TextStyle(fontSize: 20)),
+                    color: Theme.of(context).primaryColor,
+                    textColor: Colors.white,
+                    onPressed: verify,
+                    
+                   ),
+                    TextFormField(
+                    decoration: InputDecoration(
+                     
+                      border: const OutlineInputBorder(borderSide: const BorderSide(color: Colors.grey)),
+                        labelText: 'Verification Code', hintText: "XXXX", ),
+                       
+                        
+                  ),
+                  
+                  Container(
+                    
+                    margin: const EdgeInsets.only(left:10,top: 60,bottom: 70),
+                    child: Text("By creating an account you agree to our Terms of Service and Privacy Policy", style: TextStyle(fontSize:20),textAlign: TextAlign.center,),
+                    
+                  ),
+                   
+                    RaisedButton(
+                     padding: const EdgeInsets.only(left:120,right: 120),
+                   
+                    child: Text("Register", style: TextStyle(fontSize:30),),
+                    color: Theme.of(context).primaryColor,
+                    textColor: Colors.white,
+                    
+                    
+                    onPressed: verify,
+                   ),
+                  
+                   ]
+                  ))))
+
+              );}}
