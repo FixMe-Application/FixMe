abbbd9bc44513575e87f5628a9674f0602b63e9f
zeenayouhan <zinayouhan33@gmail.com>
2020-04-13 19:57:42 +0530

    Registration-fuel-3

--- lib/Register-Fuel.dart
+++ lib/Register-Fuel.dart
@@ -72,3 +72,89 @@ class _RegisterFuelState extends State<RegisterFuel> {
                     onPressed: verify,
                     
                    ),
+                       TextFormField(
+                    decoration: InputDecoration(
+                     
+                      border: const OutlineInputBorder(borderSide: const BorderSide(color: Colors.grey)),
+                        labelText: 'Verification Code', hintText: "XXXX", ),
+                       
+                        
+                  ),
+                  Row(mainAxisAlignment: MainAxisAlignment.center,
+                               children: <Widget>[
+                               
+                               Column(
+                                 mainAxisSize: MainAxisSize.min,
+                                  children: <Widget>[
+                       IconButton(
+                         icon:Icon( Icons.search,color: Colors.blue,),
+                         tooltip: "Explorer",
+                         iconSize: 60.0,
+                         onPressed: (){}
+                               ),
+                       Text("Explorer")]),
+                       Column(
+                                 mainAxisSize: MainAxisSize.min,
+                                  children: <Widget>[
+                       IconButton(
+                         icon:Icon( Icons.add_a_photo,color: Colors.blue),
+                         tooltip: "Explorer",
+                         iconSize: 60.0,
+                         onPressed: (){}
+),
+                       Text("Photos"),
+                       ]),
+                       Column(
+                                 mainAxisSize: MainAxisSize.min,
+                                  children: <Widget>[
+                       IconButton(
+                         icon:Icon( Icons.add,color: Colors.blue),
+                         tooltip: "Explorer",
+                         iconSize: 60.0,
+                         onPressed: (){}
+),
+                       Text("Photos"),
+                       ]),
+                    
+                       
+                      //  Icon(
+                      //  Icons.add_a_photo,
+                      //  color: Colors.blue,
+                      //  size: 60.0,
+                       
+                       
+                      //  ),
+                      //   Icon(
+                      //  Icons.add,
+                      //  color: Colors.blue,
+                      //  size: 60.0,
+                       
+                       
+                       
+                       
+                     ],
+                   ),
+                  Container(
+                    
+                    margin: const EdgeInsets.only(left:10,top: 60,bottom: 70),
+                    child: Text("By creating an account you agree to our Terms of Service and Privacy Policy", style: TextStyle(fontSize:20),textAlign: TextAlign.center,),
+                    
+                  ),
+                   
+                    RaisedButton(
+                     padding: const EdgeInsets.only(left:120,right: 120),
+                   
+                    child: Text("Register", style: TextStyle(fontSize:30),),
+                    color: Theme.of(context).primaryColor,
+                    textColor: Colors.white,
+                    
+                    
+                    onPressed: verify,
+                   ),
+                  
+                   ]
+                  ))))
+
+              );
+              }
+}
