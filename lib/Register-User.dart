22689d6bc91688e061796bb1cf2c1605737d6872
zeenayouhan <zinayouhan33@gmail.com>
2020-04-13 19:50:42 +0530

    Registration-user-2

--- lib/Register-User.dart
+++ lib/Register-User.dart
@@ -20,3 +20,26 @@ class MyApp extends StatelessWidget {
     );
   }
 }
+class RegisterUser extends StatefulWidget {
+   @override
+  _RegisterUserState createState() => _RegisterUserState();
+  
+
+  
+}
+class _RegisterUserState extends State<RegisterUser> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      
+      appBar: AppBar(
+        backgroundColor: Colors.lightBlueAccent,
+        title: Text("Register", style: TextStyle(fontSize: 30), ),
+        centerTitle: true,
+        ),
+        body: 
+        Container(
+          color: Colors.white,
+          
+          
+          padding: const EdgeInsets.only(left:20,top: 80,right:20),}}
