606dc47b05d9c67c76d253beef080a6a81e67a33
zeenayouhan <zinayouhan33@gmail.com>
2020-04-13 19:56:55 +0530

    Registration-fuel-2

--- lib/Register-Fuel.dart
+++ lib/Register-Fuel.dart
@@ -20,3 +20,55 @@ class MyApp extends StatelessWidget {
     );
   }
 }
+
+class RegisterFuel extends StatefulWidget {
+   @override
+  _RegisterFuelState createState() => _RegisterFuelState();
+  
+
+  
+}
+class _RegisterFuelState extends State<RegisterFuel> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      
+      appBar: AppBar(
+        backgroundColor: Colors.lightBlueAccent,
+        title: Text("Register", style: TextStyle(fontSize: 30), ),
+        centerTitle: true,
+        ),
+        body: 
+        Container(
+          color: Colors.white,
+          
+          
+          padding: const EdgeInsets.only(left:20,top: 80,right:20),
+          child:SingleChildScrollView(
+
+            child: Form(       
+               
+            
+            child: Column(
+              
+              
+                children: <Widget>[
+                  Text("Step 2/2", style: TextStyle(fontSize: 30,color: Colors.lightBlue),textAlign: TextAlign.center, ),
+                  
+                  
+                  TextFormField(
+                    
+                    
+                    decoration: InputDecoration(
+                      border: const OutlineInputBorder(borderSide: const BorderSide(color: Colors.grey,width: 1.0)),
+                      
+                        labelText: 'Phone Number', hintText: "0XXXXXXX"),
+                        
+                  ),
+                   RaisedButton(
+                    child: Text("Verify",style:TextStyle(fontSize: 20)),
+                    color: Theme.of(context).primaryColor,
+                    textColor: Colors.white,
+                    onPressed: verify,
+                    
+                   ),
