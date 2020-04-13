f55805a8b3799e787e227c531bb874bdb9b064db
zeenayouhan <zinayouhan33@gmail.com>
2020-04-13 19:39:55 +0530

    Registration-1

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,48 @@
 import 'package:flutter/material.dart';
+import './Register-Fuel.dart';
+import './Register-Garage.dart';
+import './Register-User.dart';
 
 void main() => runApp(MyApp());
 
+void verify(){
+  
+
+
+}
+
+
+ String dropdownValue = 'One';
+
 class MyApp extends StatelessWidget {
+ 
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      home: Text('Hello') ,
-   
+      title: 'LOGIN',
+      theme: ThemeData(
+        
+        primarySwatch: Colors.blue,
+      ),
+      home: Register(),
     );
   }
 }
+class Register extends StatefulWidget {
+
+
+   @override
+  _RegisterState createState() => _RegisterState();
+  
+
+  
+}
+class _RegisterState extends State<Register> { 
+ 
+
+
+  @override
+  Widget build(BuildContext context) {
+    
+  }}
