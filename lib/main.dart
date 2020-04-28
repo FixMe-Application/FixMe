dc50203119a7aa12a7a5484f952ec1fb4b2a3992
zeenayouhan <zinayouhan33@gmail.com>
2020-04-28 14:56:18 +0530

    ConfirmService-User

--- lib/main.dart
+++ lib/main.dart
@@ -5,10 +5,12 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      home: Text('Hello') ,
-   
+    return Container(
+        decoration: BoxDecoration(
+        borderRadius:BorderRadius.circular(25),
+        color:Colors.white54,
+      ),
+      padding: EdgeInsets.all(21.0),  
     );
   }
 }
