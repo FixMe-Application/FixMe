310e71e9b497b8042a85ae968e799ca4ad4cb80c
Kesavan Selvarajah <kesavan@thinksmart.com>
2021-03-28 14:54:38 +0530

    chatbot.dart file created

---
+++ lib/ChatBot/chatbot.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/cupertino.dart';
+
+class ChatBot extends StatefulWidget {
+  @override
+  _ChatBotState createState() => _ChatBotState();
+}
+
+class _ChatBotState extends State<ChatBot> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
