b82d82004eea9809dd9f429b4c4cc971fa4dbb71
Kesavan Selvarajah <kesavan@thinksmart.com>
2021-03-29 23:13:05 +0530

    added custom chat appbar

--- lib/ChatBot/chatbot.dart
+++ lib/ChatBot/chatbot.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 class ChatBot extends StatefulWidget {
   @override
@@ -8,6 +9,43 @@ class ChatBot extends StatefulWidget {
 class _ChatBotState extends State<ChatBot> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+        appBar: AppBar(
+          elevation: 0,
+          automaticallyImplyLeading: false,
+          backgroundColor: Colors.white,
+          flexibleSpace: SafeArea(
+            child: Container(
+              padding: EdgeInsets.only(right: 16),
+              child: Row(
+                children: <Widget>[
+                  IconButton(
+                    onPressed: (){
+                      Navigator.pop(context);
+                    },
+                    icon: Icon(Icons.arrow_back,color: Colors.black,),
+                  ),
+                  SizedBox(width: 2,),
+                  CircleAvatar(
+                    child: Image.asset('images/chatbot.png'),
+                    maxRadius: 20,
+                  ),
+                  SizedBox(width: 12,),
+                  Expanded(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        Text("FixMe ChatBot",style: TextStyle( fontSize: 16 ,fontWeight: FontWeight.w600),),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+        body: Container()
+    );
   }
 }
