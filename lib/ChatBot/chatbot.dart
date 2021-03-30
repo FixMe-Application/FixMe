d3ff53a8dbf77c4528ea83097014b03f91edd3c9
Kesavan Selvarajah <kesavan@thinksmart.com>
2021-03-30 22:26:43 +0530

    added chatMessage List for testing purpose

--- lib/ChatBot/chatbot.dart
+++ lib/ChatBot/chatbot.dart
@@ -1,12 +1,35 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
+import 'Models/chatMessageModel.dart';
+
 class ChatBot extends StatefulWidget {
   @override
   _ChatBotState createState() => _ChatBotState();
 }
 
 class _ChatBotState extends State<ChatBot> {
+
+
+  List<ChatMessage> messages = [
+    ChatMessage(messageContent: "Hello, I am FixMe ChatBot", messageType: "receiver"),
+    ChatMessage(messageContent: "How can I help you? Please choose one topic from below", messageType: "receiver"),
+    ChatMessage(messageContent: "1. Warning Lights", messageType: "receiver"),
+    ChatMessage(messageContent: "2. A Sputtering Engine", messageType: "receiver"),
+    ChatMessage(messageContent: "3. Poor Fuel Economy", messageType: "receiver"),
+    ChatMessage(messageContent: "4. Dead Battery", messageType: "receiver"),
+    ChatMessage(messageContent: "5. Flat Tires", messageType: "receiver"),
+    ChatMessage(messageContent: "6. Brakes Squeaking or Grinding", messageType: "receiver"),
+    ChatMessage(messageContent: "7. Alternator Failure", messageType: "receiver"),
+    ChatMessage(messageContent: "8. Broken Starter Motor", messageType: "receiver"),
+    ChatMessage(messageContent: "9. Steering Wheel Shaking", messageType: "receiver"),
+    ChatMessage(messageContent: "10. Failed Emissions Test", messageType: "receiver"),
+    ChatMessage(messageContent: "11. Overheating", messageType: "receiver"),
+    ChatMessage(messageContent: "12. Slipping Automatic Transmission", messageType: "receiver"),
+    ChatMessage(messageContent: "1", messageType: "sender"),
+    ChatMessage(messageContent: "A warning or check engine light is the most common issue for US car, truck and SUV owners. These lights illuminate when the vehicle’s ECU (engine control unit) detects an error code triggered by a sensor. Since there are more than 200 possible warning code, having a professional mechanic complete a warning light inspection is the best way to determine the source and make the right repairs.", messageType: "receiver"),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
