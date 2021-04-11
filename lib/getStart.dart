c4631b01ecefa4da6dbbc2a405839763e91256db
zeenayouhan <zinayouhan33@gmail.com>
2021-04-11 21:33:36 +0530

    made responsive for getStart

--- lib/getStart.dart
+++ lib/getStart.dart
@@ -1,57 +1,67 @@
+import 'package:fix_me_app/sizeConfig.dart';
 import 'package:flutter/material.dart';
 
 class GetStart extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    SizeConfig().init(context);
     return Scaffold(
-      backgroundColor: Colors.white,
+      backgroundColor: Colors.grey[800],
       body: new Stack(
         children: <Widget>[
           Column(
             children: <Widget>[
               new Container(
-                color: Colors.blue[100].withOpacity(0.5),
-                height: 350,
+                color: Colors.deepPurple[900],
+                height: SizeConfig.blockSizeVertical * 50,
               ),
             ],
           ),
           Positioned(
-            left: 135,
-            top: 105,
+            left: SizeConfig.left / 3,
+            right: SizeConfig.right / 3,
+            top: SizeConfig.blockSizeVertical * 20,
             child: new Text(
-              "FixMe",
+              " FixMe",
               style: new TextStyle(
-                color: Colors.blue,
-                fontSize: 50,
+                color: Colors.yellow[800],
+                fontSize: SizeConfig.screenWidth / 9,
                 fontWeight: FontWeight.bold,
               ),
             ),
           ),
           Positioned(
-              top: 235,
-              left: 95,
+              top: SizeConfig.blockSizeVertical * 35,
+              left: SizeConfig.left / 4,
               child: new Container(
-                width: 225.0,
-                height: 225.0,
+                width: SizeConfig.screenWidth / 2,
+                height: SizeConfig.screenHeight / 2,
                 decoration: new BoxDecoration(
                     shape: BoxShape.circle,
                     image: new DecorationImage(
-                        fit: BoxFit.fill,
-                        image: new AssetImage("./assets/fblogin.png"))),
+                      fit: BoxFit.fill,
+                      image: new AssetImage(
+                        "./assets/car.png",
+                      ),
+                    )),
               )),
           Positioned(
-              top: 600,
-              left: 65,
+              top: SizeConfig.blockSizeVertical * 80,
+              width: SizeConfig.screenWidth,
               child: Padding(
-                padding: EdgeInsets.symmetric(horizontal: 100, vertical: 10),
+                padding: EdgeInsets.symmetric(
+                    horizontal: SizeConfig.blockSizeHorizontal / 1,
+                    vertical: SizeConfig.blockSizeVertical * 10),
                 child: ElevatedButton(
                   onPressed: () => {"Button pressed"},
-                  style: ElevatedButton.styleFrom(primary: Colors.blue),
+                  style: ElevatedButton.styleFrom(
+                    primary: Colors.blue[900],
+                  ),
                   child: new Text(
                     "Let's Start",
                     style: new TextStyle(
-                      color: Colors.white,
-                      fontSize: 16.9,
+                      color: Colors.yellow[800],
+                      fontSize: SizeConfig.screenWidth / 10,
                       fontWeight: FontWeight.w600,
                     ),
                   ),
