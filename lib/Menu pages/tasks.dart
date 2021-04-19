0c358bc46f59c4037fa86c15dd3296842fdae3b6
zeenayouhan <zinayouhan33@gmail.com>
2021-04-19 22:53:33 +0530

    Responsive screen for tasks

--- lib/Menu pages/tasks.dart
+++ lib/Menu pages/tasks.dart
@@ -1,3 +1,4 @@
+import 'package:fix_me_app/sizeConfig.dart';
 import 'package:flutter/material.dart';
 import 'package:fix_me_app/widgets/header.dart';
 
@@ -11,43 +12,70 @@ class TasksPage extends StatefulWidget {
 class _TasksPageState extends State<TasksPage> {
   @override
   Widget build(BuildContext context) {
+    SizeConfig().init(context);
     return Scaffold(
-      appBar: header(titleText: "Tasks"),
-       body: DefaultTabController(
-         length: 3,
-         child: Column(
-          children: <Widget>[
-            Container(
-              constraints: BoxConstraints.expand(height: 50),
-              child: TabBar(
-                labelColor: Colors.black,
-                labelStyle: TextStyle(
-                  fontWeight: FontWeight.bold
+        appBar: AppBar(
+            backgroundColor: Colors.lightBlue[900],
+            title: Text(
+              "Tasks",
+              style: TextStyle(
+                  fontSize: SizeConfig.screenWidth / 15,
+                  color: Colors.yellow[700]),
+              textAlign: TextAlign.center,
+            )),
+        body: Container(
+          color: Colors.grey[800],
+          child: DefaultTabController(
+            length: 3,
+            child: Column(
+              children: <Widget>[
+                Container(
+                  constraints: BoxConstraints.expand(height: 50),
+                  child: TabBar(
+                      labelColor: Colors.white,
+                      labelStyle: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: SizeConfig.screenHeight / 40,
+                          color: Colors.white),
+                      tabs: [
+                        Tab(text: "All"),
+                        Tab(text: "Completed"),
+                        Tab(text: "Cancelled")
+                      ]),
                 ),
-                tabs: [
-                Tab(text: "All"),
-                Tab(text: "Completed"),
-                Tab(text: "Cancelled")
-              ]),
+                Expanded(
+                  child: Container(
+                    child: TabBarView(children: [
+                      Container(
+                        child: Text(
+                          "All Body",
+                          style: TextStyle(
+                              fontSize: SizeConfig.screenHeight / 40,
+                              color: Colors.white),
+                        ),
+                      ),
+                      Container(
+                        child: Text(
+                          "Completed Body",
+                          style: TextStyle(
+                              fontSize: SizeConfig.screenHeight / 40,
+                              color: Colors.white),
+                        ),
+                      ),
+                      Container(
+                        child: Text(
+                          "Cancelled Body",
+                          style: TextStyle(
+                              fontSize: SizeConfig.screenHeight / 40,
+                              color: Colors.white),
+                        ),
+                      ),
+                    ]),
+                  ),
+                )
+              ],
             ),
-            Expanded(
-              child: Container(
-                child: TabBarView(children: [
-                  Container(
-                    child: Text("All Body"),
-                  ),
-                  Container(
-                    child: Text("Completed Body"),
-                  ),
-                  Container(
-                    child: Text("Cancelled Body"),
-                  ),
-                ]),
-              ),
-            )
-          ],
-        ),
-       ),
-    );
+          ),
+        ));
   }
 }
