fba05d330b8f36268027a52b19658fdbb4d5e23e
Ashen Malaka <ashenmalaka56@gmail.com>
2021-05-24 01:09:35 +0530

    Modified 'Register' Page for 'Mechanics'

--- lib/Mechanic/Register/register-garage.dart
+++ lib/Mechanic/Register/register-garage.dart
@@ -37,48 +37,93 @@ class _RegisterGarageState extends State<RegisterGarage> {
     SizeConfig().init(context);
     return Scaffold(
         appBar: AppBar(
-          backgroundColor: Colors.lightBlue[900],
+          backgroundColor: Color(0xff2a2e43),
           title: Text(
             "Register",
-            style: TextStyle(fontSize: 30, color: Colors.yellow[700]),
+            style: TextStyle(
+              fontSize: SizeConfig.screenWidth / 20,
+              color: Colors.white,
+            ),
           ),
           centerTitle: true,
         ),
         body: Container(
             height: SizeConfig.screenHeight,
-            color: Colors.grey[800],
-            padding: const EdgeInsets.only(left: 20, top: 80, right: 20),
+            color: Colors.grey[700],
+            padding: const EdgeInsets.only(left: 20, top: 50, right: 20),
             child: SingleChildScrollView(
                 child: Form(
                     child: Column(children: <Widget>[
               Text(
                 "Step 2/2",
                 style: TextStyle(
-                    fontSize: SizeConfig.screenWidth / 15,
-                    color: Colors.yellow[700]),
+                  fontSize: SizeConfig.screenWidth / 15,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white,
+                ),
                 textAlign: TextAlign.center,
               ),
+              SizedBox(
+                height: 20.0,
+              ),
               TextFormField(
+                style: TextStyle(
+                  color: Colors.white,
+                ),
                 decoration: InputDecoration(
                   border: const OutlineInputBorder(
-                      borderSide:
-                          const BorderSide(color: Colors.grey, width: 1.0)),
+                    borderSide:
+                        const BorderSide(color: Colors.grey, width: 1.0),
+                  ),
+                  enabledBorder: OutlineInputBorder(
+                    borderSide: BorderSide(
+                      color: Color(0xff2a2e43),
+                      width: 2.0,
+                    ),
+                  ),
                   labelText: 'Phone Number',
                   hintText: "0XXXXXXX",
                   labelStyle: TextStyle(color: Colors.white),
                   hintStyle: TextStyle(color: Colors.white),
                 ),
               ),
-              RaisedButton(
-                  child: Text("Verify",
-                      style: TextStyle(fontSize: SizeConfig.screenWidth / 20)),
-                  color: Colors.lightBlue[900],
-                  textColor: Colors.yellow[700],
+              SizedBox(
+                height: 20.0,
+              ),
+              TextButton(
+                  style: TextButton.styleFrom(
+                    primary: Colors.white,
+                    backgroundColor: Color(0xff2a2e43),
+                    onSurface: Colors.grey,
+                    minimumSize: Size(200.0, 50.0),
+                    shape: const RoundedRectangleBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(20.0)),
+                    ),
+                  ),
+                  child: Text(
+                    "Verify",
+                    style: TextStyle(
+                      fontSize: SizeConfig.screenWidth / 20,
+                    ),
+                  ),
                   onPressed: () {}),
+              SizedBox(
+                height: 20.0,
+              ),
               TextFormField(
+                style: TextStyle(
+                  color: Colors.white,
+                ),
                 decoration: InputDecoration(
                   border: const OutlineInputBorder(
-                      borderSide: const BorderSide(color: Colors.grey)),
+                    borderSide: const BorderSide(color: Colors.grey),
+                  ),
+                  enabledBorder: OutlineInputBorder(
+                    borderSide: BorderSide(
+                      color: Color(0xff2a2e43),
+                      width: 2.0,
+                    ),
+                  ),
                   labelText: 'Verification Code',
                   hintText: "XXXX",
                   labelStyle: TextStyle(color: Colors.white),
@@ -89,13 +134,17 @@ class _RegisterGarageState extends State<RegisterGarage> {
                 margin: EdgeInsets.all(10),
               ),
               DropdownButton<String>(
-                  dropdownColor: Colors.grey[800],
                   isExpanded: true,
                   value: dropdownValue,
-                  iconSize: 29,
+                  dropdownColor: Color(0xff2a2e43),
+                  icon: Icon(
+                    Icons.arrow_drop_down,
+                    color: Colors.white,
+                  ),
+                  iconSize: 42,
                   elevation: 2,
                   style: TextStyle(
-                      fontSize: SizeConfig.screenWidth / 20,
+                      fontSize: SizeConfig.screenWidth / 21,
                       color: Colors.white),
                   onChanged: (String newValue) {
                     setState(() {
@@ -109,6 +158,9 @@ class _RegisterGarageState extends State<RegisterGarage> {
                       child: Text(value),
                     );
                   }).toList()),
+              SizedBox(
+                height: 20.0,
+              ),
               Row(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: <Widget>[
@@ -116,22 +168,26 @@ class _RegisterGarageState extends State<RegisterGarage> {
                     IconButton(
                         icon: Icon(
                           Icons.search,
-                          color: Colors.yellow[700],
+                          color: Color(0xff2a2e43),
                         ),
                         tooltip: "Explorer",
                         iconSize: 60.0,
                         onPressed: () async {
                           result = await Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                  builder: (context) => LocationSelection()));
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => LocationSelection(),
+                            ),
+                          );
                           setState(() {
                             selectedLocation = result[0];
                             selectedLocationText = result[1];
                           });
                         }),
-                    Text(selectedLocationText,
-                        style: TextStyle(color: Colors.white))
+                    Text(
+                      selectedLocationText,
+                      style: TextStyle(color: Colors.white),
+                    )
                   ]),
                 ],
               ),
@@ -140,21 +196,29 @@ class _RegisterGarageState extends State<RegisterGarage> {
                 child: Text(
                   "By creating an account you agree to our Terms of Service and Privacy Policy",
                   style: TextStyle(
-                      fontSize: SizeConfig.screenWidth / 20,
+                      fontSize: SizeConfig.screenWidth / 25,
                       color: Colors.white),
                   textAlign: TextAlign.center,
                 ),
               ),
               Align(
                 alignment: Alignment.bottomCenter,
-                child: RaisedButton(
-                    padding: const EdgeInsets.only(left: 120, right: 120),
+                child: TextButton(
+                    style: TextButton.styleFrom(
+                      primary: Colors.white,
+                      backgroundColor: Color(0xff2a2e43),
+                      onSurface: Colors.grey,
+                      minimumSize: Size(200.0, 50.0),
+                      shape: const RoundedRectangleBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(20.0)),
+                      ),
+                    ),
                     child: Text(
                       "Submit",
-                      style: TextStyle(fontSize: SizeConfig.screenWidth / 20),
+                      style: TextStyle(
+                        fontSize: SizeConfig.screenWidth / 20,
+                      ),
                     ),
-                    color: Colors.lightBlue[900],
-                    textColor: Colors.yellow[700],
                     onPressed: () async {
                       try {
                         User user = await _auth.registerWithEmailAndPassword(
